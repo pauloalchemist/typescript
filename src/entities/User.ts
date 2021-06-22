99d403b4a20623fdb47636edb2336739a0ca7b0c
pauloalchemist <paulolinsdev@gmail.com>
2021-06-22 19:35:27 -0300

    add entidade User

---
+++ src/entities/User.ts
@@ -0,0 +1,15 @@
+import { randomUUID } from 'crypto'
+
+class User {
+  id: string
+  name: string
+  email: string
+
+  constructor () {
+    if (!this.id) {
+      this.id = randomUUID()
+    }
+  }
+} 
+
+export { User }
